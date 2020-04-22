d9cb995ff5d8f2f9a0b25c4e35b1f5bcc2189646
joseph kigin <joseph.kigin@gmail.com>
2020-04-22 14:59:27 -0500

    Errors route to the error page correctly when not in a Development environment.

--- Armis.BusinessModels/QualityModels/Spec/SpecRevModel.cs
+++ Armis.BusinessModels/QualityModels/Spec/SpecRevModel.cs
@@ -12,7 +12,7 @@ namespace Armis.BusinessModels.QualityModels.Spec
         public string ExternalRev { get; set; }
         public short EmployeeNumber { get; set; } //TODO: this could be an entire employee model but for now I am just keeping it as a number because all the employee information isn't needed when the spec rev is pulled.
         public DateTime DateModified { get; set; }
-        public TimeSpan TimeModified { get; set; }
+        public TimeSpan TimeModified { get; set; } //TODO: This might not be used, come back to check if it is still needed.  4/22/2020
         public IEnumerable<SpecSubLevelModel> SubLevels { get; set; }
     }
 }
